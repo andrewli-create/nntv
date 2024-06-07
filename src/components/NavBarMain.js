95d4b81fdfda3251277cef5a956852a6199c047b
andrewli <andrewli.create@gmail.com>
2024-06-07 04:47:13 -0400

    added mobile nav bar

--- src/components/NavBarMain.js
+++ src/components/NavBarMain.js
@@ -53,7 +53,41 @@ const NavBarMain = ({data, preview}) => {
             } */}
             <img className="obj-fit-contain" src={brandLogo} alt={brandLogoAlt} style={{ width: "88px" }} /> 
           </Link>
-          
+          <ul
+          id="navMenu-mobile"
+          // className={` navbar-start has-text-centered navbar-menu ${isActive && "is-active"}`}
+          className={`nav-bar-link-wrapper-mobile ${isActive && "is-active"}`}
+          >
+            {/* TODO: inline override of padding is a result of refactoring to a ul for accessibilty purposes, would like to see a css re-write that makes this unneccesary.*/}
+            {navItem.map((item, index) => (
+              item.navItemToggle ? (
+                  <li key={index} className="navbar-item">
+                    <Link className="navbar-item-link d-flex d-flex-c d-flex-col" to={item.pageURL}>
+                      {item.pageName}
+                    </Link>
+                    {item.subNavItem ? 
+                      (
+                        <ul className="sub-navbar sub-navbar-hidden preview-hover">
+                          {item.subNavItem
+                            ? (item.subNavItem.map((itemInner, index) => (
+                              <li className={itemInner.subPageName.length < 15 ? "sub-navbar-item" : "sub-navbar-item-wide"}>
+                                <Link className="d-flex d-flex-c d-flex-col" to={itemInner.subPageURL}>
+                                  {itemInner.subPageName}
+                                </Link>
+                              </li>
+                            )))
+                            : null
+                          }
+                        </ul>
+                      )
+                      : 
+                      null
+                    }
+                  </li>
+                )
+              : null
+            ))}
+          </ul>
           {/* Hamburger menu */}
           <h1></h1>
           <button
@@ -69,7 +103,7 @@ const NavBarMain = ({data, preview}) => {
         <ul
           id="navMenu"
           // className={` navbar-start has-text-centered navbar-menu ${isActive && "is-active"}`}
-          className={`nav-bar-link-wrapper ${isActive && "is-active"}`}
+          className={`nav-bar-link-wrapper`}
         >
           {/* TODO: inline override of padding is a result of refactoring to a ul for accessibilty purposes, would like to see a css re-write that makes this unneccesary.*/}
           {navItem.map((item, index) => (
@@ -100,7 +134,6 @@ const NavBarMain = ({data, preview}) => {
               )
             : null
           ))}
-          
         </ul>
         <div className={'nav-sign-up-button-wrapper button-disable-appearance'}>
           <button className={'nav-sign-up-button'}>
