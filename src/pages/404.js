03d39088c215dac54141d58dc84c538b58035af7
andrewli <andrewli.create@gmail.com>
2024-06-11 04:54:31 -0400

    revampted 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,9 +3,20 @@ import Layout from "../components/Layout";
 
 const NotFoundPage = () => (
   <Layout>
-    <div>
+    {/* <div>
       <h1>NOT FOUND</h1>
       <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </div> */}
+    <div className="display-flex d-flex-c d-flex-row" style={{minHeight: "60vh"}}>
+      <div className="display-flex d-flex-c d-flex-col">
+        <div style={{width: "100%", padding: "0 35px"}} className="display-flex d-flex-c d-flex-col">
+          <h1 style={{marginBottom: "20px"}}>Sorry, the page you are looking for does not exist.</h1>
+          <p style={{marginBottom: "20px"}}>Please check if the URL you typed is correct.</p>
+          <a href="/">
+            <button className="button-generic">Back to Home Page</button>
+          </a>
+        </div>
+      </div>
     </div>
   </Layout>
 );
